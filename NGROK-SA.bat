555d0946a6d4d4e7cdf6f487e57abd1e9c09ad90
bEmcho-cyber <80263692+bEmcho-cyber@users.noreply.github.com>
2021-03-11 11:15:56 +0200

    Add files via upload

---
+++ NGROK-SA.bat
@@ -0,0 +1,5 @@
+C:
+cd C:\Users\circleci\.ngrok2
+echo region: sa >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
+copy ngrok.yml C:\Windows\System32
+nssm install ngrok C:\Windows\System32\ngrok.exe start --all --config="C:\Windows\System32\ngrok.yml"
